0ddd32c59f06ffaf7ed66f7e634a98ca9ad2e47d
americ-anfootball <128167529+americ-anfootball@users.noreply.github.com>
2023-07-05 18:03:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,157 +1,100 @@
 import tkinter as tk
-from tkinter import filedialog
-import geopandas as gpd
-import json
-from file_utils import import_data, read_file, open_file_dialog, add_files, on_select, load_vector_data, set_working_object_a, set_working_object_b
-from color_window import ColorWindow
-from color_utils import generate_color_ramp, generate_and_display_color_palette, generate_styled_geojson
-import matplotlib.pyplot as plt
-import pandas as pd
-from matplotlib.colors import ListedColormap
-from branca.colormap import LinearColormap
-import folium
-import json
-import numpy as np
-import webbrowser
-from folium_window import FoliumWindow
-#import threading
-#from multiprocessing import Process
+from file_utils import add_files, on_select, set_working_object_a, set_working_object_b
+from color_window import ColorWindowLogic, ColorWindowGUI
+from folium_window import FoliumWindowLogic, FoliumWindowGUI
 
-class App:
 
-    #def on_close(self):
-        ## Wait for all threads to finish
-        #for thread in threading.enumerate():
-            #if thread is not threading.current_thread():
-                #thread.join()
-        
-        ## Terminate all processes
-        #print(f"Terminating {len(self.my_processes)} processes")
-        #for i, process in enumerate(self.my_processes):
-            #process.terminate()
-            #print(f"Terminated {i+1} out of {len(self.my_processes)} processes")
-        
-        ## Destroy the root window
-        #self.root.destroy()
-    
-    def add_files_command(self):
-        add_files(self.listbox)
-        
-    def on_select_command(self, event):
-        on_select(self.listbox, self.property_listbox)
-        
-    def set_working_object_a_command(self):
-        self.working_object_a = set_working_object_a(self.listbox, self.text_widget_a)
-
-    def set_working_object_b_command(self):
-        self.working_object_b = set_working_object_b(self.property_listbox, self.text_widget_b)
-
+class AppLogic:
     def __init__(self):
-        self.root = tk.Tk()
-        self.root.title('Foliumizer')
-        self.root.geometry("625x400")
         self.working_object_a = None
         self.working_object_b = None
         self.selected_property = None
-        self.color_ramp = None
-        self.bins = 5
 
+    def add_files(self, listbox):
+        add_files(listbox)
+
+    def on_select(self, listbox, property_listbox):
+        on_select(listbox, property_listbox)
+
+    def set_working_object_a(self, listbox, text_widget_a):
+        self.working_object_a = set_working_object_a(listbox, text_widget_a)
+
+    def set_working_object_b(self, property_listbox, text_widget_b):
+        self.working_object_b = set_working_object_b(property_listbox, text_widget_b)
+
+
+class AppGUI:
+    def __init__(self, logic):
+        self.logic = logic
+        self.root = tk.Tk()
+        self.root.title('Foliumizer')
+        self.root.geometry("675x525")
+
+        # create and configure widgets
         # add titles above the listboxes
-        self.file_label = tk.Label(self.root, text="Files")
-        self.property_label = tk.Label(self.root, text="Properties")
+        self.file_label = tk.Label(self.root, text="Loaded Files")
+        self.property_label = tk.Label(self.root, text="Properties of Selected File")
 
-        # set the exportselection option of the listbox to False
-        self.listbox = tk.Listbox(self.root, exportselection=False)
+		# create the listboxes
+        self.listbox = tk.Listbox(self.root, height=15, width=40, exportselection=False)
+        self.property_listbox = tk.Listbox(self.root, height=15, width=40, exportselection=False)
 
+		# create the buttons
         self.add_button = tk.Button(self.root, text="Add Files", command=self.add_files_command)
+        self.set_button = tk.Button(self.root, text="Set Spatial Data of Working Object", command=self.set_working_object_a_command)
+        self.set_button_b = tk.Button(self.root, text="Set Active Property of Working Object", command=self.set_working_object_b_command)
+        self.generate_color_button = tk.Button(self.root, text="Generate Color Palette", command=self.open_color_window)
+        self.folium_button = tk.Button(self.root, text="Open Folium Window", command=self.open_folium_window, state=tk.DISABLED)
 
+		# create the text widgets
+        self.text_widget_a = tk.Text(self.root, height=5, width=40)
+        self.text_widget_b = tk.Text(self.root, height=5, width=40)
+
+		# bind events to the listbox
         self.listbox.bind("<<ListboxSelect>>", self.on_select_command)
 
-        # set the exportselection option of the property_listbox to False
-        self.property_listbox = tk.Listbox(self.root, exportselection=False)
+		# use the grid geometry manager to position the widgets in the window
+        self.add_button.grid(row=0, column=0, columnspan=6, padx=5, pady=5)
+        self.file_label.grid(row=2, column=1, columnspan=1, padx=5, pady=5) 
+        self.listbox.grid(row=3, column=1, columnspan=1, padx=5, pady=5)
+        self.set_button.grid(row=4, column=1, columnspan=1, padx=5, pady=5)
+        self.property_label.grid(row=2, column=4, columnspan=1, padx=5, pady=5)
+        self.property_listbox.grid(row=3, column=4, columnspan=1, padx=5, pady=5)
+        self.set_button_b.grid(row=4, column=4, columnspan=1, padx=5, pady=5)
+        self.text_widget_a.grid(row=5, column=1, columnspan=1, padx=5, pady=5)
+        self.text_widget_b.grid(row=5, column=4, columnspan=1, padx=5, pady=5)
+        self.generate_color_button.grid(row=6, column=0, columnspan=6, padx=5, pady=5)
+        self.folium_button.grid(row=7, column=0, columnspan=6)
 
-        self.set_button = tk.Button(self.root, text="Set Spatial Data of Working Object", command=self.set_working_object_a_command)
-        self.set_button.grid(row=2, column=0)
+    def add_files_command(self):
+        self.logic.add_files(self.listbox)
 
-        self.set_button_b = tk.Button(self.root, text="Set Property of Working Object", command=self.set_working_object_b_command)
-        self.set_button_b.grid(row=2, column=1)
+    def on_select_command(self, event):
+        self.logic.on_select(self.listbox, self.property_listbox)
 
-        # add two Text widgets to display the messages
-        self.text_widget_a = tk.Text(self.root, height=5, width=40)
-        self.text_widget_a.grid(row=3, column=0)
+    def set_working_object_a_command(self):
+        self.logic.set_working_object_a(self.listbox, self.text_widget_a)
 
-        self.text_widget_b = tk.Text(self.root, height=5, width=40)
-        self.text_widget_b.grid(row=3, column=1)
-
-        # add a button to generate a color palette
-        self.generate_color_button = tk.Button(self.root, text="Generate Color Palette", command=self.open_color_window)
-
-        # use the grid geometry manager to position the widgets in the window
-        self.file_label.grid(row=0, column=0)
-        self.property_label.grid(row=0, column=1)
-        self.listbox.grid(row=1, column=0, padx=5)
-        self.property_listbox.grid(row=1, column=1, padx=5)
-        self.add_button.grid(row=2, column=0, columnspan=2)
-        self.set_button.grid(row=2, column=0, padx=5)
-        self.set_button_b.grid(row=2, column=1, padx=5)
-        self.generate_color_button.grid(row=4, column=0, columnspan=2)
-
-        # Create a Button widget for opening the folium window
-        self.folium_button = tk.Button(self.root, text="Open Folium Window", command=self.open_folium_window, state=tk.DISABLED)
-        self.folium_button.grid(row=5, column=0, columnspan=2)
-
-        ## Initialize the my_processes attribute with an empty list
-        #self.my_processes = []
-
-        ## add a protocol handler for the WM_DELETE_WINDOW event that calls the destroy method of the root window
-        #self.root.protocol("WM_DELETE_WINDOW", self.on_close)
-        
-        self.root.mainloop()
-    
-        # Create a color ramp listbox
-        self.color_ramp_listbox = tk.Listbox(self.root, exportselection=False)
-        self.color_ramp_listbox.grid(row=1, column=2)
-
-        # Bind the <<ListboxSelect>> event of the color ramp listbox to the set_color_ramp method
-        self.color_ramp_listbox.bind("<<ListboxSelect>>", lambda event: self.set_color_ramp())
-
-    def create_process(self, target_function):
-        # Create a new process
-        new_process = Process(target=target_function)
-        
-        # Add the new process to the my_processes list
-        self.my_processes.append(new_process)
-        
-        # Start the new process
-        new_process.start()
+    def set_working_object_b_command(self):
+        self.logic.set_working_object_b(self.property_listbox, self.text_widget_b)
 
     def open_color_window(self):
-        # Get the selected column name from the second listbox
-        selected_column_indices = self.property_listbox.curselection()
-        if not selected_column_indices:
-            message = "Please select a property"
-            print(message)
-            # insert message into text_widget instead of printing it to console
-            self.text_widget_b.insert(tk.END, message + "\n")
-            return
-        selected_column_index = selected_column_indices[0]
-        selected_column_name = self.property_listbox.get(selected_column_index)
+        color_logic = ColorWindowLogic(working_object_a=self.logic.working_object_a, working_object_b=self.logic.working_object_b)
+        color_window = ColorWindowGUI(master=self, logic=color_logic)
+        color_window.mainloop()
 
-        # Access the value of the listbox, text_widget_a, and property_listbox attributes from the self object
-        listbox = self.listbox
-        text_widget_a = self.text_widget_a
-        text_widget_b = self.text_widget_b
-        property_listbox = self.property_listbox
-
-        # Call the set_working_object_a and set_working_object_b functions with the appropriate arguments
-        working_object_a = set_working_object_a(listbox, text_widget_a)
-        working_object_b = set_working_object_b(property_listbox, text_widget_b)
-
-        # Create an instance of the ColorWindow class and pass the working objects as arguments
-        color_window = ColorWindow(self.root, working_object_a, working_object_b)
-  
     def open_folium_window(self):
-        #Open a new folium window
-        FoliumWindow(self.root, self, self.working_object_a, self.working_object_b)
-        
-app = App()
+        # Create an instance of the FoliumWindowLogic class
+        folium_logic = FoliumWindowLogic(working_object_a=self.logic.working_object_a, working_object_b=self.logic.working_object_b)
+
+        # Create an instance of the FoliumWindowGUI class
+        folium_window = FoliumWindowGUI(master=self.root, logic=folium_logic)
+
+        # Store a reference to the folium window as an instance attribute
+        self.folium_window = folium_window
+
+
+if __name__ == "__main__":
+    logic = AppLogic()
+    gui = AppGUI(logic)
+    gui.root.mainloop()
